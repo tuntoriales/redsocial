8c0eebba848bf3f0f7016c20509b8b45d7a8c46e
Tuntoriales <tuntoriales8a@gmail.com>
2017-06-09 20:00:27 -0500

    Parte 5 Tutorial RED SOCIAL Tuntoriales

---
+++ publicaciones.php
@@ -0,0 +1,112 @@
+<?php
+include 'lib/config.php';
+$CantidadMostrar=5;
+     // Validado  la variable GET
+    $compag         =(int)(!isset($_GET['pag'])) ? 1 : $_GET['pag']; 
+	$TotalReg       =mysql_query("SELECT * FROM publicaciones");
+	$totalr = mysql_num_rows($TotalReg);
+	//Se divide la cantidad de registro de la BD con la cantidad a mostrar 
+	$TotalRegistro  =ceil($totalr/$CantidadMostrar);
+	 //Operacion matematica para mostrar los siquientes datos.
+	$IncrimentNum =(($compag +1)<=$TotalRegistro)?($compag +1):0;
+	//Consulta SQL
+	$consultavistas ="SELECT *
+				FROM
+				publicaciones
+				ORDER BY
+				id_pub DESC LIMIT ".(($compag-1)*$CantidadMostrar)." , ".$CantidadMostrar;
+	$consulta=mysql_query($consultavistas);
+	while ($lista=mysql_fetch_array($consulta)) {
+
+		$userid = mysql_real_escape_string($lista['usuario']);
+
+		$usuariob = mysql_query("SELECT * FROM usuarios WHERE id_use = '$userid'");
+		$use = mysql_fetch_array($usuariob);
+	?>
+	<!-- START PUBLICACIONES -->
+          <!-- Box Comment -->
+          <div class="box box-widget">
+            <div class="box-header with-border">
+              <div class="user-block">
+                <img class="img-circle" src="avatars/<?php echo $use['avatar']; ?>" alt="User Image">
+                <span class="description" onclick="location.href='perfil.php?id=<?php echo $use['id_use'];?>';" style="cursor:pointer; color: #3C8DBC;""><?php echo $use['usuario'];?></span>
+                <span class="description"><?php echo $lista['fecha'];?></span>
+              </div>
+              <!-- /.user-block -->
+              <div class="box-tools">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+              <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <!-- post text -->
+              <p><?php echo $lista['contenido'];?></p>
+
+              <?php 
+              if($lista['imagen'] != '')
+              {
+              ?>
+              <img src="publicaciones/<?php echo $lista['imagen'];?>" width="50%">
+              <?php
+          	  }
+          	  ?>
+
+              <br><br>
+
+              <!-- Social sharing buttons -->
+              <button type="button" class="btn btn-default btn-xs"><i class="fa fa-share"></i> Compartir</button>
+              <button type="button" class="btn btn-default btn-xs"><i class="fa fa-thumbs-o-up"></i> Me gusta</button>
+              <span class="pull-right text-muted">45 likes - 2 comments</span>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer box-comments">
+              <div class="box-comment">
+                <!-- User image -->
+                <img class="img-circle img-sm" src="dist/img/user3-128x128.jpg" alt="User Image">
+
+                <div class="comment-text">
+                      <span class="username">
+                        Maria Gonzales
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                </div>
+                <!-- /.comment-text -->
+              </div>
+              <!-- /.box-comment -->
+              <div class="box-comment">
+                <!-- User image -->
+                <img class="img-circle img-sm" src="dist/img/user5-128x128.jpg" alt="User Image">
+
+                <div class="comment-text">
+                      <span class="username">
+                        Nora Havisham
+                        <span class="text-muted pull-right">8:03 PM Today</span>
+                      </span><!-- /.username -->
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using
+                  'Content here, content here', making it look like readable English.
+                </div>
+                <!-- /.comment-text -->
+              </div>
+              <!-- /.box-comment -->
+
+              </div>
+
+        </div>
+        <!-- /.col -->
+        <!-- END PUBLICACIONES -->
+    
+    <br><br>
+
+	<?php
+	}
+	//Validmos el incrementador par que no genere error
+	//de consulta.  
+    if($IncrimentNum<=0){}else {
+	echo "<a href=\"publicaciones.php?pag=".$IncrimentNum."\">Seguiente</a>";
+	}
+?>
